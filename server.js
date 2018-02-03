f357d61a3e9c49b91689dbeb0031bab6de2bfe92
Elliot Kim <elliotkim916@gmail.com>
2018-02-02 17:15:53 -0800

    added comments

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+// importing blogpostsRouter
 const blogpostsRouter = require('./blogpostsRouter');
 
+// any requests to /blog-posts should be routed to blogpostsRouter
 app.use('/blog-posts', blogpostsRouter);
 
 app.listen(8080, () => {
