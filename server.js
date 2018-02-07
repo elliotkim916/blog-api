bb00bd400cbebbe5c2e7550ecc8e9de2812cc923
Elliot Kim <elliotkim916@gmail.com>
2018-02-06 16:10:17 -0800

    added chai, and added runServer & closeServer function

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
+// log the http layer
+app.use(morgan('common'));
 
 // importing blogpostsRouter
 const blogpostsRouter = require('./blogpostsRouter');
@@ -7,6 +12,35 @@ const blogpostsRouter = require('./blogpostsRouter');
 // any requests to /blog-posts should be routed to blogpostsRouter
 app.use('/blog-posts', blogpostsRouter);
 
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080');
-});
+let server;
+
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve(server);
+        }).on('error', err => {
+            reject(err)
+        });
+    });
+}
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
